b2c24d4ea31aca7d7b4a71443b86f1bd4512c9ae
Ubuntu <ubuntu@ip-172-31-1-81.ec2.internal>
2026-05-24 10:46:02 +0000

    working

--- Shristi/src/main/java/com/etc/Shristi/config/CorsConfig.java
+++ Shristi/src/main/java/com/etc/Shristi/config/CorsConfig.java
@@ -1,35 +1,37 @@
-//package com.etc.Shristi.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class CorsConfig {
-//
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//
-//        return new WebMvcConfigurer() {
-//
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//
-//                registry.addMapping("/**")
-//                        .allowedOrigins(
-//                                "http://localhost:5173",
-//                                "http://myapp.local"
-//                        )
-//                        .allowedMethods(
-//                                "GET",
-//                                "POST",
-//                                "PUT",
-//                                "DELETE",
-//                                "OPTIONS"
-//                        )
-//                        .allowedHeaders("*");
-//            }
-//        };
-//    }
-//}
+package com.etc.Shristi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "http://myapp.local",
+                                "http://3.231.55.224:8080"
+                        )
+                        .allowedMethods(
+                                "GET",
+                                "POST",
+                                "PUT",
+                                "DELETE",
+                                "OPTIONS"
+                        )
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
